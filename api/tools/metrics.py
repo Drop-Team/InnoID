ba77e81e69a2b2361fc4970e47a4f96a6857ff69
Egor <edkuz11@yandex.ru>
2022-03-26 17:49:50 +0300

    Apps & users count update time adding

--- api/tools/metrics.py
+++ api/tools/metrics.py
@@ -4,8 +4,10 @@ from api.database.db_session import create_session
 from api.models.apps import App
 from api.models.users import User
 
-users_total = Gauge("users_total", "Total users count")
-apps_total = Gauge("apps_total", "Total apps count")
+from datetime import datetime
+
+users_total = Gauge("users_total", "Total users count", ["update_time"])
+apps_total = Gauge("apps_total", "Total apps count", ["update_time"])
 start_time = Gauge("start_time", "Start time")
 apps_requests = Counter("apps_requests", "Requests by apps", ["id", "name", "path", "status"])
 
@@ -13,13 +15,13 @@ apps_requests = Counter("apps_requests", "Requests by apps", ["id", "name", "pat
 def update_users_count_metric():
     session = create_session()
     users_count = len(session.query(User).all())
-    users_total.set(users_count)
+    users_total.labels(datetime.now().timestamp()).set(users_count)
 
 
 def update_apps_count_metric():
     session = create_session()
     apps_count = len(session.query(App).all())
-    apps_total.set(apps_count)
+    apps_total.labels(datetime.now().timestamp()).set(apps_count)
 
 
 def register_startup_metrics():
