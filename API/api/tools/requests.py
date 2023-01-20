9ee0094108a9374efecf516feb4d1c0d26f8286c
Egor Kuziakov <edkuz11@yandex.ru>
2023-01-20 21:56:50 +0300

    Now app is not required for getting users

--- API/api/tools/requests.py
+++ API/api/tools/requests.py
@@ -3,7 +3,7 @@ from functools import wraps
 from flask import request
 
 from api.database.db_session import create_session
-from api.models.apps import App
+from api.models.apps import App, APIPermissions
 from api.tools import errors
 from api.tools.metrics import apps_requests
 
@@ -28,16 +28,24 @@ def auth_check(required_permission: int):
             token = get_token(request.headers.get("Authorization", ""))
             app = get_app_by_token(token) if token else None
             if not token or not app:
-                raise errors.NoAuthError
+                app_permissions = encode_numbers([APIPermissions.get_user])
+                allowed_user_fields = encode_numbers([1])
+                app_id = "unknown"
+                app_name = "unknown"
+            else:
+                app_permissions = app.api_permissions
+                allowed_user_fields = app.allowed_user_fields
+                app_id = app.id
+                app_name = app.name
 
-            metric_labels = [app.id, app.name, request.path]
+            metric_labels = [app_id, app_name, request.path]
 
-            if required_permission not in decode_number(app.api_permissions):
+            if required_permission not in decode_number(app_permissions):
                 apps_requests.labels(*metric_labels, "Access denied").inc()
                 raise errors.AccessDeniedError
 
             apps_requests.labels(*metric_labels, "Success").inc()
-            return func(*args, **kwargs, user_fields=decode_number(app.allowed_user_fields))
+            return func(*args, **kwargs, user_fields=decode_number(allowed_user_fields))
 
         return decorator
 
