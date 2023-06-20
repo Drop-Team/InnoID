61f817633fbb2bc2a89dcedc7a147ba7b7933d35
Egor Kuziakov <edkuz11@yandex.ru>
2023-06-20 13:58:14 +0300

    (Core) Add app_id and user_id dependencies

--- Core/innoid_core/api/v2/dependencies/apps.py
+++ Core/innoid_core/api/v2/dependencies/apps.py
@@ -1,3 +1,4 @@
+import uuid
 from typing import Annotated
 
 from fastapi import Depends
@@ -12,13 +13,21 @@ from .use_cases import get_app_api_key_use_case, get_app_use_case
 api_key_scheme = APIKeyHeader(name="X-API-Key", auto_error=False)
 
 
-def get_current_app(
+def get_current_app_id(
         api_key: Annotated[str, Depends(api_key_scheme)],
         app_api_key_use_case: Annotated[AppApiKeyUseCase, Depends(get_app_api_key_use_case)],
+) -> uuid.UUID | None:
+    try:
+        return app_api_key_use_case.authenticate(api_key)
+    except DomainError:
+        return None
+
+
+def get_current_app(
+        app_id: Annotated[uuid.UUID, Depends(get_current_app_id)],
         app_use_case: Annotated[AppUseCase, Depends(get_app_use_case)],
 ) -> App | None:
     try:
-        app_id = app_api_key_use_case.authenticate(api_key)
         return app_use_case.get_by_id(app_id)
     except DomainError:
         return None
--- Core/innoid_core/api/v2/dependencies/users.py
+++ Core/innoid_core/api/v2/dependencies/users.py
@@ -1,13 +1,25 @@
+import uuid
 from typing import Annotated
 
 from fastapi import Depends
 
 from api.v2.dependencies.use_cases import get_user_use_case
+from domain.errors import DomainError
 from domain.user.entities import User
 from domain.user.usecases import UserUseCase
 
 
+def get_current_user_id(
+
+) -> uuid.UUID | None:
+    return None
+
+
 def get_current_user(
+        user_id: Annotated[uuid.UUID | None, Depends(get_current_user_id)],
         user_use_case: Annotated[UserUseCase, Depends(get_user_use_case)],
 ) -> User | None:
-    return None
+    try:
+        return user_use_case.get_by_id(user_id)
+    except DomainError:
+        return None
