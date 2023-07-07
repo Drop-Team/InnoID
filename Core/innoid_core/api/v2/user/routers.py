eb0f2423aa9cd8ebc99d0ec8ab5f1a9d73bed6bf
Egor Kuziakov <egor.kuziakov@gmail.com>
2023-07-07 18:49:27 +0300

    (Core) Add current user endpoint

--- Core/innoid_core/api/v2/user/routers.py
+++ Core/innoid_core/api/v2/user/routers.py
@@ -4,12 +4,21 @@ from typing import Annotated
 from fastapi import APIRouter, Depends
 
 from api.v2.dependencies.use_cases import get_user_use_case
+from api.v2.dependencies.users import get_current_user
 from domain.user import errors as domain_errors
+from domain.user.entities import User
 from domain.user.usecases import UserUseCase
 from . import errors as api_errors
 from . import models as api_models
 
 users_router = APIRouter(prefix="/users", tags=["users"])
+
+
+@users_router.get("/me", response_model=api_models.User)
+def get_current_active_user(user: Annotated[User, Depends(get_current_user)]):
+    if not user:
+        raise api_errors.UserNotFoundApiError()
+    return api_models.User(user_id=user.user_id, email=user.email)
 
 
 @users_router.get("/{user_id}", response_model=api_models.User)
